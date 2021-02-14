ee2ef0299deadb6939f42ff7d69bd7baf138ac24
Dev2Jobs <retefsf1234fas@aol.com>
2021-02-15 03:12:17 +0900

    complete login1view

--- t2102141334ecom/01_Login1/Login1View.swift
+++ t2102141334ecom/01_Login1/Login1View.swift
@@ -14,57 +14,66 @@ import UIKit
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 class Login1View: UIViewController {
 
-	@IBOutlet var labelTitle: UILabel! = UILabel()
-	@IBOutlet var labelSubTitle: UILabel! = UILabel()
+	@IBOutlet var labelTitle: UILabel!
+	@IBOutlet var labelSubTitle: UILabel!
 	@IBOutlet var imageViewLogo: UIImageView!
-	@IBOutlet var textFieldEmail: UITextField! = UITextField()
-	@IBOutlet var textFieldPassword: UITextField! = UITextField()
+	@IBOutlet var textFieldEmail: UITextField!
+	@IBOutlet var textFieldPassword: UITextField!
 	@IBOutlet var buttonHideShowPassword: UIButton!
 
 	//---------------------------------------------------------------------------------------------------------------------------------------------
 	override func viewDidLoad() {
+        ZmFunc().dlog("Login1View - viewDidLoad")
 
 		super.viewDidLoad()
 
-//		textFieldEmail.setLeftPadding(value: 15)
-//		textFieldPassword.setLeftPadding(value: 15)
-//		textFieldPassword.setRightPadding(value: 40)
+        if (nil == textFieldEmail) {
+            ZmFunc().dlog("AppDelegate  - textFieldEmail - error")
+        }else {
+            ZmFunc().dlog("AppDelegate - textFieldEmail - yes")
+        }
+        
+		textFieldEmail.setLeftPadding(value: 15)
+		textFieldPassword.setLeftPadding(value: 15)
+		textFieldPassword.setRightPadding(value: 40)
 
 		loadData()
 	}
 
 	// MARK: - Data methods
-	//---------------------------------------------------------------------------------------------------------------------------------------------
+
 	func loadData() {
+        ZmFunc().dlog("Login1View - loadData")
 
 		labelTitle.text = "Welcome to\nAppDesignKit"
 		labelSubTitle.text = "An exciting place for the whole family to shop."
 	}
 
 	// MARK: - User actions
-	//---------------------------------------------------------------------------------------------------------------------------------------------
+
 	@IBAction func actionHideShowPassword(_ sender: Any) {
+        ZmFunc().dlog("Login1View - actionHideShowPassword")
 
 		buttonHideShowPassword.isSelected = !buttonHideShowPassword.isSelected
 		textFieldPassword.isSecureTextEntry = !buttonHideShowPassword.isSelected
 	}
 
-	//---------------------------------------------------------------------------------------------------------------------------------------------
 	@IBAction func actionLogin(_ sender: Any) {
+        ZmFunc().dlog("Login1View - actionLogin")
 
 		print(#function)
 		dismiss(animated: true)
 	}
 
-	//---------------------------------------------------------------------------------------------------------------------------------------------
 	@IBAction func actionForgotPassword(_ sender: Any) {
+        ZmFunc().dlog("Login1View - actionForgotPassword")
 
 		print(#function)
 		dismiss(animated: true)
 	}
 
-	//---------------------------------------------------------------------------------------------------------------------------------------------
 	@IBAction func actionSignUp(_ sender: Any) {
+        ZmFunc().dlog("Login1View - actionSignUp")
 
 		print(#function)
 		dismiss(animated: true)
--- t2102141334ecom/AppDelegate.swift
+++ t2102141334ecom/AppDelegate.swift
@@ -8,34 +8,63 @@
 import UIKit
 import IQKeyboardManagerSwift
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-//@UIApplicationMain
+//@main
 //class AppDelegate: UIResponder, UIApplicationDelegate {
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var Login1View: Login1View!
+//    var baseView: BaseView!
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        ZmFunc().dlog("AppDelegate - UIApplication.LaunchOptionsKey")
 
         window = UIWindow(frame: UIScreen.main.bounds)
-
+        
+//        baseView = BaseView(nibName: "BaseView", bundle: nil)
+//        let navController = NavigationController(rootViewController: baseView)
+        
         Login1View = t2102141334ecom.Login1View(nibName: "Login1View", bundle: nil)
         let navController = NavigationController(rootViewController: Login1View)
+        
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
 
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         
+        ZmFunc().dlog("AppDelegate - UIApplication.LaunchOptionsKey - 22")
+        
         return true
     }
+//=================================
+    func applicationWillResignActive(_ application: UIApplication) {
+        ZmFunc().dlog("AppDelegate - applicationWillResignActive")
+    }
 
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        ZmFunc().dlog("AppDelegate - applicationDidEnterBackground")
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        ZmFunc().dlog("AppDelegate - applicationWillEnterForeground")
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        ZmFunc().dlog("AppDelegate - applicationDidBecomeActive")
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        ZmFunc().dlog("AppDelegate - applicationWillTerminate")
+    }
+//=================================
     // MARK: UISceneSession Lifecycle
-
+/*
     @available (iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -49,7 +78,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+*/
 }
 
